5cd03d6f457a4d641209a39489c59a96de644c16
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-13 16:27:02 +0800

    implement session data caching

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,10 +1,22 @@
-let age = prompt("Пожалуйста, введите ваш возраст");
+let userData = new Map();
+userData.set("agent", window.navigator.userAgent);
 
+let age = prompt("Пожалуйста, введите ваш возраст");
+userData.set("age", age);
+
+let dateNow;
 if (age >= 18) {
-	alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+	dateNow = new Date().toLocaleString();
+	alert("Приветствуем на LifeSpot! " + dateNow);
 } else {
 	alert(
 		"Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены"
 	);
 	window.location.href = "http://www.google.com";
+}
+
+userData.set("startDate", dateNow);
+
+for (let result of userData) {
+	console.log(result);
 }
