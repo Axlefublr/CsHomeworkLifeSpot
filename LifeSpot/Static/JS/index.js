379bacb38c78911a963516efd147b1bd9648dc4e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-13 17:47:57 +0800

    move code into their own functions

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,22 +1,35 @@
-let userData = new Map();
-userData.set("agent", window.navigator.userAgent);
-
-let age = prompt("Пожалуйста, введите ваш возраст");
-userData.set("age", age);
-
-let dateNow;
-if (age >= 18) {
-	dateNow = new Date().toLocaleString();
-	alert("Приветствуем на LifeSpot! " + dateNow);
-} else {
-	alert(
-		"Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены"
-	);
-	window.location.href = "http://www.google.com";
+function filterLivestreams(input) {
+	input = input.toLowerCase();
+	let elements = document.getElementsByClassName("video-container");
+	for (let i = 0; i < elements.length; i++) {
+		let element = elements[i].getElementsByTagName("h3")[0];
+		let videoDescription = element.innerText.toLowerCase();
+		if (!videoDescription.includes(input)) {
+			elements[i].style.display = "none";
+		} else {
+			elements[i].style.display = "inline-block";
+		}
+	}
 }
 
-userData.set("startDate", dateNow);
+function handleSession() {
+	let userData = new Map();
+	userData.set("agent", window.navigator.userAgent);
 
-for (let result of userData) {
-	console.log(result);
+	let age = prompt("Пожалуйста, введите ваш возраст");
+	userData.set("age", age);
+	if (age >= 18) {
+		let dateNow = new Date().toLocaleString();
+		userData.set("startDate", dateNow);
+		alert("Приветствуем на LifeSpot! " + dateNow);
+	} else {
+		alert(
+			"Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены"
+		);
+		window.location.href = "http://www.google.com";
+	}
+	for (let result of userData) {
+		console.log(result);
+	}
+	return userData;
 }
