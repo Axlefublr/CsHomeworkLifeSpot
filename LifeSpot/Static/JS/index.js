d07ed71292b1496ff1cea97e005d6612b9df9bd6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-21 06:23:34 +0800

    cleanup

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,58 +1,51 @@
-﻿/*
- * Сессия теперь создается в общей области видимости.
- * Будет "захватываться" тремя функциями
- *
- * */
-let session = new Map();
+﻿function handleSession(logger, checker) {
+	if (window.sessionStorage.getItem("startDate") == null) {
+		window.sessionStorage.setItem("startDate", new Date().toLocaleString());
+	}
 
-/*
- * Сохранение данных сессии сразу при заходе пользователя на страницу
- *
- * */
-function handleSession() {
-	// Сохраним время начала сессии
-	session.set("startDate", new Date().toLocaleString());
-	// Сохраним UserAgent
-	session.set("userAgent", window.navigator.userAgent);
+	if (window.sessionStorage.getItem("userAgent") == null) {
+		window.sessionStorage.setItem("userAgent", window.navigator.userAgent);
+	}
+
+	if (window.sessionStorage.getItem("userAge") == null) {
+		let input = prompt("Пожалуйста, введите ваш возраст?");
+		window.sessionStorage.setItem("userAge", input);
+
+		checker(true);
+	} else {
+		checker(false);
+	}
+
+	logger();
 }
 
-/*
- * Проверка возраста пользователя
- *
- * */
-function checkAge() {
-	session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
-
-	if (session.get("age") >= 18) {
-		alert(
-			"Приветствуем на LifeSpot! " +
-				"\n" +
-				"Текущее время: " +
-				new Date().toLocaleString()
-		);
+let checker = function (newVisit) {
+	if (window.sessionStorage.getItem("userAge") >= 18) {
+		if (newVisit) {
+			alert(
+				"Приветствуем на LifeSpot! " +
+					"\n" +
+					"Текущее время: " +
+					new Date().toLocaleString()
+			);
+		}
 	} else {
 		alert(
-			"Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены"
+			"Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены"
 		);
 		window.location.href = "http://www.google.com";
 	}
-}
-
-/*
- * Вывод данных сессии в консоль
- *
- * */
-let sessionLog = function () {
-	for (let result of session) {
-		console.log(result);
-	}
 };
 
-/*
- * Функция для фильтраци контента
- * Будет вызываться благодаря атрибуту oninput на index.html
- *
- * */
+let logger = function () {
+	console.log("Начало сессии: " + window.sessionStorage.getItem("startDate"));
+	console.log(
+		"Даныне клиента: " + window.sessionStorage.getItem("userAgent")
+	);
+	console.log(
+		"Возраст пользователя: " + window.sessionStorage.getItem("userAge")
+	);
+};
 
 function filterContent() {
 	let elements = document.getElementsByClassName("video-container");
@@ -72,10 +65,6 @@ function filterContent() {
 	}
 }
 
-/*
- * Всплывающее окно будет показано по таймауту
- *
- * */
 // setTimeout(() =>
 //     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
 // 7000);
