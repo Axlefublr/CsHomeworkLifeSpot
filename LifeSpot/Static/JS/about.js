494baba6e4ddbac7a7ce94cb6c432129ba262685
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-22 06:48:26 +0800

    implement commenting system

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,25 +1,41 @@
-﻿function getReview() {
-	let review = {};
+﻿function getComment() {
+	let comment = {};
 
-	review["userName"] = prompt("Как вас зовут?");
-	if (review["userName"] == null) {
+	comment.author = prompt("Как вас зовут ?");
+	if (comment.author == null) {
 		return;
 	}
 
-	review["comment"] = prompt("Напишите свой отзыв");
-	if (review["comment"] == null) {
+	comment.text = prompt("Оставьте отзыв");
+	if (comment.text == null) {
 		return;
 	}
 
-	review["date"] = new Date().toLocaleString();
+	comment.date = new Date().toLocaleString();
 
-	writeReview(review);
+	let enableLikes = confirm("Разрешить пользователям оценивать ваш отзыв?");
+
+	if (enableLikes) {
+		let review = Object.create(comment);
+		review.rate = 0;
+		writeReview(review);
+	} else {
+		writeReview(comment);
+	}
 }
 
 const writeReview = (review) => {
+	let likeCounter = "";
+
+	if (review.hasOwnProperty("rate")) {
+		likeCounter +=
+			'           <b style="color: chocolate">Рейтинг:</b>   ' +
+			review.rate;
+	}
+
 	document.getElementsByClassName("reviews")[0].innerHTML +=
 		'    <div class="review-text">\n' +
-		`<p> <i> <b>${review["userName"]}</b>  ${review["date"]}</i></p>` +
-		`<p>${review["comment"]}</p>` +
+		`<p> <i> <b>${review["author"]}</b>  ${review["date"]}${likeCounter}</i></p>` +
+		`<p>${review["text"]}</p>` +
 		"</div>";
 };
