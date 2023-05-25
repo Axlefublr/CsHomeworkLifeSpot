ff0aa38a93636de9f9489b71534edf7cc9dc951f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-25 17:20:27 +0800

    implement button that saves input

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -14,3 +14,19 @@ Array.from(myMap, ([key, value]) =>
 	newArrayOfStrings.push(`${key} - ${value}`)
 );
 console.log(newArrayOfStrings);
+
+const saveInput = function () {
+	let currentInput = document
+		.getElementsByTagName("input")[0]
+		.value.toLowerCase();
+
+	alert(
+		"Последний ввод: " +
+			this.lastInput +
+			"\n" +
+			"Текущий ввод: " +
+			currentInput
+	);
+
+	this.lastInput = currentInput;
+};
